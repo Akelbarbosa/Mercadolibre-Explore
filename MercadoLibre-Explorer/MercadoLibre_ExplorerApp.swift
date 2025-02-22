84c73e5f9c5d3a59124e3c00906e90a1e5030fdf
Akel Barbosa <akelbarbosa@gmail.com>
2025-02-21 22:33:26 -0500

    Initial Commit

---
+++ MercadoLibre-Explorer/MercadoLibre_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MercadoLibre_ExplorerApp.swift
+//  MercadoLibre-Explorer
+//
+//  Created by Akel Barbosa on 21/02/25.
+//
+
+import SwiftUI
+
+@main
+struct MercadoLibre_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
